f84ca58650d3621f4175689d5c572e866d6e942a
ilabstaff <ilabstaff@172.16.116.79>
2019-08-19 17:48:36 -0700

    ArrayCheatSheet, RobotRace

--- src/_00_IntroToArrays/_00_ArrayCheatSheet.java
+++ src/_00_IntroToArrays/_00_ArrayCheatSheet.java
@@ -5,29 +5,105 @@ import java.util.Random;
 public class _00_ArrayCheatSheet {
 	public static void main(String[] args) {
 		//1. make an array of 5 Strings
+		
+		String [] arr= new String [5];
+		
 
 		//2. print the third element in the array
+		
+		System.out.println(arr[3]);
+		
 
 		//3. set the third element to a different value
-
+		
+			arr[3] = "waffles";
+			
+			
 		//4. print the third element again
+			
+			System.out.println(arr[3]);
+			
 		
 		//5. use a for loop to set all the elements in the array to a string of your choice
+			
+			for (int i = 0; i < arr.length; i++) {
+				
+				
+				arr[i]="cheese";
+				
+			}
+			
 		
 		//6. use a for loop to print all the values in the array
 		//   BE SURE TO USE THE ARRAY'S length VARIABLE
+			
+			for (int i = 0; i < arr.length; i++) {
+				
+				System.out.println(arr[i]);
+				
+			}
+			
 		
 		//7. make an array of 50 integers
+			
+			int [] ray = new int [50];
+			
 
 		//8. use a for loop to make every value of the integer array a random number
+			
+			Random ran = new Random();
+			
+			
+			for (int i = 0; i < ray.length; i++) {
+				
+				ray[i]= ran.nextInt();
+				
+			}
+			
 
 		//9. without printing the entire array, print only the smallest number on the array
+			
+			int smallest = ray[0];
+			
+			for (int i = 1; i < ray.length; i++) {
+				
+				if(ray[i]<smallest) {
+					
+					ray[i]=smallest;
+					
+				}
+				
+			}
+			
+			System.out.println(smallest);
+			
 
 		//10 print the entire array to see if step 8 was correct
+			for (int j = 0; j < ray.length; j++) {
+				
+				System.out.println(ray[j]);
+			
+			}
+			
 
 		//11. print the largest number in the array.
+			
+			int biggest= ray[0];
+			
+			for (int i = 0; i < ray.length; i++) {
+				
+				if(ray[i]>biggest) {
+					
+					ray[i]=biggest;
+					
+				}
+				
+				
+			}
 		
 		//12. print only the last element in the array
 		
+			System.out.println(ray.length-1);
+			
 	}
 }
--- src/_00_IntroToArrays/_01_RobotRace.java
+++ src/_00_IntroToArrays/_01_RobotRace.java
@@ -9,22 +9,110 @@ import org.jointheleague.graphical.robot.Robot;
 public class _01_RobotRace {
 	//1. make a main method
 	
+		public static void main(String[] args) {
+			
+		
 		//2. create an array of 5 robots.
-
+		
+		Robot [] cookie = new Robot[5];
+		
+		
 		//3. use a for loop to initialize the robots.
+		
+		
+		for (int i = 0; i < cookie.length; i++) {
+			
+			cookie[i]= new Robot();
+			cookie[i].moveTo(i*200, 250);
+			
+		}
+		
 
-			//4. make each robot start at the bottom of the screen, side by side, facing up
+		//4. make each robot start at the bottom of the screen, side by side, facing up
+		
+		
+		
+		
 	
 		//5. use another for loop to iterate through the array and make each robot move 
 	    //   a random amount less than 50.
+		
+		
+		
+		
+		
     	
 		//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
+		
+		Random ran = new Random();
+
+		boolean race= true;
+		
+		while(race==true){
+			
+			for (int i = 0; i < cookie.length; i++) {
+			
+			cookie[i].move(ran.nextInt(50));
+			
+			
+			if(cookie[i].getY()<0){
+				
+				JOptionPane.showMessageDialog(null, "The winner is robot # " + i);
+				
+				race=false;
+						
+				break;
+						
+			}
+			
+			
+				
+			}
+				
+			
+			
+		}
 
 		//7. declare that robot the winner and throw it a party!
     	
 		//8. try different races with different amounts of robots.
+		
+		Robot [] cake = new Robot [4];
+		
+		for (int i = 0; i < cake.length; i++) {
+			
+			cake[i]= new Robot();
+			cake[i].moveTo(i*200, 250);
+			
+		}
+		
+		Random dom = new Random();
+		
+		boolean go = true;
+		
+		while(go==true) {
+			
+			for (int i = 0; i < cake.length; i++) {
+				
+				cake[i].move(dom.nextInt(50));
+				
+				if(cake[i].getY()<0) {
+					
+					JOptionPane.showMessageDialog(null, "The winner is Robot # " + i);
+					
+					go=false;
+					break;
+					
+				}
+				
+			}
+			
+		}
+		
+		
     	
 	    //9. make the robots race around a circular track.
-
+		
+		}
 	
 }
