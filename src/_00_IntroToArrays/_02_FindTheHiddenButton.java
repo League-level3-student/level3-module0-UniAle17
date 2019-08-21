22f8517f5ab7747c15ee2beca0839fe6b6fe7daa
ilabstaff <ilabstaff@172.16.116.68>
2019-08-20 18:23:17 -0700

    hidden buttons array methods

--- src/_00_IntroToArrays/_02_FindTheHiddenButton.java
+++ src/_00_IntroToArrays/_02_FindTheHiddenButton.java
@@ -1,5 +1,6 @@
 package _00_IntroToArrays;
 
+import java.awt.Button;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Random;
@@ -17,7 +18,12 @@ public class _02_FindTheHiddenButton implements ActionListener{
 	
 	//1. create an array of JButtons. Don't initialize it yet.
 	
+	JButton [] buttons;
+	
 	//2 create an integer variable called hiddenButton
+	
+	int hiddenButton;
+	
 	
 	public static void main(String[] args) {
 		new _02_FindTheHiddenButton().start();
@@ -28,30 +34,77 @@ public class _02_FindTheHiddenButton implements ActionListener{
 		panel = new JPanel();
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		//3.  Ask the user to enter a positive number and convert it to an int
+		//3.  Ask the user to enter a positive number and convert it to an int\
+		
+		String number = JOptionPane.showInputDialog("Enter a positive number");
+		
+		int num = Integer.parseInt(number);
+		
 		
 		//4. Initialize the array of JButtons to be the size of the int created in step 3
 		
+		buttons = new JButton [num];
+		
 		//5. Make a for loop to iterate through the JButton array
+		
+			for (int i = 0; i < buttons.length; i++) {
+				
+				buttons[i]= new JButton();
+				buttons[i].addActionListener(this);
+				panel.add(buttons[i]);
+				
+				
+			}
+		
 			//6. initialize each JButton in the array
 			//7. add the ActionListener to each JButton
 			//8. add each JButton to the panel
 		
 		//9 add the panel to the window
+			
+			window.add(panel);
+			
 		
 		//10. call setExtendedState(JFrame.MAXIMIZED_BOTH) on your JFrame object.
+			
+			window.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		
 		//11. set the JFrame to visible.
+			
+			window.setVisible(true);
+			
 		
 		//12. Give the user the instructions for the game.
+			
+			JOptionPane.showMessageDialog(null, "Watch carefully for the 'Me', and click the button where it appears.");
+			
 		
 		//13. initialize the hiddenButton variable to a random number less than the int created in step 3
 		
+			Random ran = new Random();
+			
+			hiddenButton=ran.nextInt(num);
+			
 		//14. Set the text of the JButton located at hiddenButton to read "ME"
+			
+			buttons[hiddenButton].setText("Me");
 
 		//15. Use Thread.sleep(100); to pause the program.
+			
+			
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
+			
 		
 		//16. Set the text of the JButton located at hiddenButton to be blank.
+			
+			buttons[hiddenButton].setText(null);
 		
 	}
 
@@ -60,7 +113,22 @@ public class _02_FindTheHiddenButton implements ActionListener{
 		JButton buttonClicked = (JButton)e.getSource();
 		
 		//17. if the hiddenButton is clicked, tell the user that they win.
+		
+		
+		if(buttonClicked==(buttons[hiddenButton])) {
+			
+			JOptionPane.showMessageDialog(null, "You are right!!!");
+			
+		}
 
 		//18. else tell them to try again
+		
+		else {
+			
+			JOptionPane.showMessageDialog(null, "Wrong button! Try again!");
+			
+		}
+		
+		
 	}
 }
--- src/_01_Simple_Array_Algorithms/_00_1D_Array_Methods.java
+++ src/_01_Simple_Array_Algorithms/_00_1D_Array_Methods.java
@@ -5,14 +5,62 @@ public class _00_1D_Array_Methods {
 	//   of the integers in the array being passed in
 	public static int sumIntArray(int[] values) {
 
-		return 0;
+		int sum=values[0];
+		
+		
+		
+		for (int i = 0; i < values.length; i++) {
+			
+			if(i==values.length-1) {
+			
+			return sum;
+				
+			}
+			
+			else{
+				sum =sum+values[i+1];
+			}
+			
+			
+			
+		}
+		
+		return sum;
+		
 	}
 	
 	//2. Complete the method so that it returns the average of all
 	//   of the integers in the array being passed in
 	public static double averageIntArray(int[] values) {
 				
-		return 0;
+
+		double sum=values[0];
+		double average=0;
+		
+		
+		for (int i = 0; i < values.length; i++) {
+			
+			if(i==values.length-1) {
+			
+			return average;
+				
+			}
+			
+			else{
+				sum =sum+values[i+1];
+				average=sum/values.length;
+			}
+			
+			
+			
+		}
+		
+		return average;
+		
+	
+		
+		
+		
 	}
 	
 	
@@ -20,6 +68,21 @@ public class _00_1D_Array_Methods {
 	//   array contains the value specified by the second parameter.
 	//   It should otherwise return false.
 	public static boolean containsIntValue(int[] array, int value) {
+		
+		for (int i = 0; i < array.length; i++) {
+			
+			if(array[i]==value) {
+				
+				return true;
+				
+			}
+			
+			else {
+				return false;
+			}
+			
+		}
+		
 	
 		return false;
 	}
@@ -28,6 +91,20 @@ public class _00_1D_Array_Methods {
 	//   first instance that the specified value occurs in the array.
 	//   If the array does not contain the specified value, it should return -1.
 	public static int getIndex(int[] arr, int value) {
+		
+		for (int i = 0; i < arr.length; i++) {
+			
+			if(arr[i]==value) {
+				
+				return arr[i];
+				
+			}
+			
+			else {
+				return -1;
+			}
+			
+		}
 		
 		return 0;
 	}
